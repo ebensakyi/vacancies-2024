f25e65b3d69e43aab2cc295a590c1f50e3d2e20f
EBENEZER AGYEMANG SAKYI <ebensakyi@gmail.com>
2024-03-20 16:43:28 +0000

    update

--- constants/index.ts
+++ constants/index.ts
@@ -3,6 +3,6 @@
 export const logo = "/img/logo.png"
 const dev = process.env.NODE_ENV !== 'production';
 
-export const SERVER_BASE_URL = dev ? 'http://localhost:3000' : 'https://vacancies.waecgh.org'
+export const SERVER_BASE_URL = dev ? 'https://vacancies.waecgh.org' : 'https://vacancies.waecgh.org'
 export const LOGIN_URL = dev ? "/auth/login" : "https://vacancies.waecgh.org/auth/login"
 export const ADMIN_LOGIN_URL = dev ? "/auth/admin/login" : "https://vacancies.waecgh.org/auth/admin/login"
